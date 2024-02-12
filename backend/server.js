956ad88802ac15a1bc9cedbb0e01a157ca72a560
Ahmed Emad <ahmedemad56565@gmail.com>
2024-02-12 16:55:05 +0200

    preparing app for production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import { products } from './data/products.js';
 const app = express();
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import { products } from './data/products.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, '../', 'dist')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +20,10 @@ app.get('/api/v1/products', (req, res) => {
 app.get('/api/v1/products/:id', (req, res) => {
   const product = products.find((p) => p.id === req.params.id);
   res.status(200).json(product);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 });
 
 app.listen(5100, () => {
